f054b0bf876e60d4df514480e2e0ea3a3117dcba
afeiship <1290657123@qq.com>
2019-04-23 11:21:42 +0800

    add: gem

---
+++ db/migrate/20190423032002_create_rails_admin_users_users.rails_admin_users.rb
@@ -0,0 +1,14 @@
+# This migration comes from rails_admin_users (originally 20190422015647)
+class CreateRailsAdminUsersUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rails_admin_users_users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :auth_token
+
+      t.timestamps
+    end
+    add_index :rails_admin_users_users, :auth_token, unique: true
+  end
+end
